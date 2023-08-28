1e9161fe297f3ee28199a985cf8f86596dfe0277
BrucePang <125526597+bruce-pang@users.noreply.github.com>
2023-08-28 23:09:38 +0800

    1-初始化工程 2-Java是如何构造链表的01

---
+++ src/main/java/com/brucepang/charpter1_linklist/level1/BasicLink0.java
@@ -0,0 +1,55 @@
+package com.brucepang.charpter1_linklist.level1;
+
+/**
+ *  一个简单的链表实例，用于演示JVM怎么构造链表的
+ *  BrucePang
+ */
+public class BasicLink0 {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6};
+        // 创建链表
+        Node node = creatLinkedList(arr);
+        System.out.println(node);
+    }
+
+    /**
+     * 通过数组创建链表
+     * @param arr 被传入的数组元素
+     * @return
+     */
+    private static Node creatLinkedList(int[] arr) {
+        // 创建表头
+        Node head = null; // 由于链表是由头节点查找下一个节点的，所以这里单独定义一个头节点，以保证每次都能使用到头节点
+
+        // 如果按照正序去构造节点,当前节点可能不知道下一个节点是null还是节点，所以这里使用倒序的方式去构造节点:
+        // 先构造最后一个节点，然后再构造倒数第二个节点，以此类推
+        // 此处是为了演示原理,所以先用硬编码,因为开发人员知道构造了一个多长的数组
+        Node node5 = new Node(arr[5]);
+        Node node4 = new Node(arr[4]);
+        Node node3 = new Node(arr[3]);
+        Node node2 = new Node(arr[2]);
+        Node node1 = new Node(arr[1]);
+        Node node0 = new Node(arr[0]);
+
+        // 构造节点之间的关系
+        head = node0;
+        node0.next = node1;
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = null; // 最后一个节点的下一个节点是null
+
+        return head; // 返回头节点,debug时可以通过头节点去查看整个链表的结构
+    }
+
+    static class Node{ // 链表的节点:由于重点是学习算法，所以这里不考虑使用面向对象的getter与setter，直接用有参构造器完成每个节点的初始化，简化代码，使用对象.属性的方式直接赋值
+        int val; // 节点的值
+        Node next; // 下一个节点的引用
+
+        public Node(int val){
+            this.val = val;
+            next = null;
+        }
+    }
+}
