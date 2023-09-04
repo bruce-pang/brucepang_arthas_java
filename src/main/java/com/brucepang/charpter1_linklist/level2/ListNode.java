efd5141b9497ca3ec5673f5f2664d4ae8ca31b30
BrucePang <125526597+bruce-pang@users.noreply.github.com>
2023-09-04 22:30:14 +0800

    1-新增结点ListNode类 2-两个链表第一个公共子节点两种解法

---
+++ src/main/java/com/brucepang/charpter1_linklist/level2/ListNode.java
@@ -0,0 +1,16 @@
+package com.brucepang.charpter1_linklist.level2;
+
+/**
+ * 在算法中最常用的链表定义方式(LeetCode中的链表定义方式)
+ *
+ * @author BrucePang
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
